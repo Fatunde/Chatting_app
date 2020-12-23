d13ba2028f9cd322c40c7443f8aca3259c822cdf
unknown <fatundedamilare@gmail.com>
2020-12-23 13:28:11 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import './App.css';
-import Join from './components/Join/Join';
+
 import Chat from './components/Chat/Chat';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Join from './components/Join/Join';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
-   
-    return ( 
-      <Router>
-        <Route exact path="/" component={Join}/>
-        <Route  path="/chat" component={Chat}/>
-  </Router>
-     );
-  
+  return (
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
 }
-
 
 export default App;
